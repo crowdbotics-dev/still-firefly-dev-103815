645a5901f2ed9ea57a28f4daeb0e72abd7fd8c1f
Crowdbotics <engineering@crowdbotics.com>
2023-07-21 10:48:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-firefly-dev-103815",
+  url: "https://still-firefly-dev-103815.botics.co",
+  api: "https://still-firefly-dev-103815.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
